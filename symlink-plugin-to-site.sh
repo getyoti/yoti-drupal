28825f7873e576487c18cc51c9b1dc97bdd7420e
Omari Rodney <omari@xchangebit.com>
2017-01-19 20:43:52 +0000

    A Drupal 7 plugin fo rYoti connect

---
+++ symlink-plugin-to-site.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+#####################
+# this script symlinks to the plugin dirs to the correct places in joomla in order to build
+#####################
+BASE=$1
+
+if [ ! -d "$BASE" ]; then
+    echo "$BASE not found."
+    echo "$0 <drupal site>"
+    exit
+fi
+
+target="$PWD/yoti_connect"
+link="$BASE/sites/all/modules/yoti_connect"
+
+# if link already exists then don't create
+if [ ! -L "$link" ]; then
+
+    # if already installed plugin then move old dir
+    if [ -d "$link" ]; then mv "$link" "$link.old"; fi
+
+    # create link
+    ln -s "$target" "$link"
+fi
+
+# add sdk
+target=$(realpath ../../src)
+link="$link/sdk"
+if [ ! -L "$link" ]; then
+    ln -s "$target" "$link"
+fi
