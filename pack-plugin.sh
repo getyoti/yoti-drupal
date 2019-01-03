32f922b5e2167faf97da8515639b600fea8945a5
Moussa Sidibe <moussa.sidibe@yoti.com>
2019-01-03 17:26:25 +0000

    SDK-729: Change TAG to SDK_TAG

--- pack-plugin.sh
+++ pack-plugin.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 NAME="yoti-for-drupal-7.x-1.x-edge.zip"
 
-TAG=$1
-DEFAULT_TAG='1.2.1'
+SDK_TAG=$1
+DEFAULT_SDK_TAG='1.2.1'
 SDK_RELATIVE_PATH="sdk"
 
-if [ "$TAG" = "" ]; then
-    TAG=$DEFAULT_TAG
+if [ "$SDK_TAG" = "" ]; then
+    SDK_TAG=$DEFAULT_SDK_TAG
 fi
 
-echo "Pulling tag $TAG.zip ..."
+echo "Pulling PHP SDK TAG $SDK_TAG.zip ..."
 
-curl https://github.com/getyoti/yoti-php-sdk/archive/$TAG.zip -O -L
-unzip $TAG.zip -d sdk
-mv sdk/yoti-php-sdk-$TAG/src/* sdk
-rm -rf sdk/yoti-php-sdk-$TAG
+curl https://github.com/getyoti/yoti-php-sdk/archive/$SDK_TAG.zip -O -L
+unzip $SDK_TAG.zip -d sdk
+mv sdk/yoti-php-sdk-$SDK_TAG/src/* sdk
+rm -rf sdk/yoti-php-sdk-$SDK_TAG
 
 if [ ! -d "./yoti" ]; then
     echo "ERROR: Must be in directory containing ./yoti folder"
