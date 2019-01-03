ed636c5314dac3932dbb89936030d5bdd73357ea
Moussa Sidibe <moussa.sidibe@yoti.com>
2019-01-03 16:57:28 +0000

    D8-SDK-729: Add the ability to pull a specific PHP SDK version

--- pack-plugin.sh
+++ pack-plugin.sh
@@ -1,10 +1,20 @@
 #!/bin/bash
+
 NAME="yoti-for-drupal-8.x-1.x-edge.zip"
+SDK_TAG=$1
+DEFAULT_SDK_TAG="1.2.1"
 SDK_RELATIVE_PATH="sdk"
-curl https://github.com/getyoti/yoti-php-sdk/archive/master.zip -O -L
-unzip master.zip -d sdk
-mv sdk/yoti-php-sdk-master/src/* sdk
-rm -rf sdk/yoti-php-sdk-master
+
+if [ "$SDK_TAG" = "" ]; then
+    SDK_TAG=$DEFAULT_SDK_TAG
+fi
+
+echo "Pulling PHP SDK TAG $SDK_TAG.zip ..."
+
+curl https://github.com/getyoti/yoti-php-sdk/archive/$SDK_TAG.zip -O -L
+unzip $SDK_TAG.zip -d sdk
+mv sdk/yoti-php-sdk-$SDK_TAG/src/* sdk
+rm -rf sdk/yoti-php-sdk-$SDK_TAG
 
 if [ ! -d "./yoti" ]; then
     echo "ERROR: Must be in directory containing ./yoti folder"
