651ee8aac6e9a20b233e83c79a79980bfdcd74ae
David Grayston <davidgrayston@googlemail.com>
2019-02-19 18:26:08 +0000

    SDK-787: Skip NULL attributes

--- yoti/YotiHelper.php
+++ yoti/YotiHelper.php
@@ -501,7 +501,7 @@ class YotiHelper {
       ];
 
       foreach($profile->getAttributes() as $attrName => $attrObj) {
-          if (in_array($attrName, $excludedAttrs)) {
+          if (in_array($attrName, $excludedAttrs) || $attrObj === NULL) {
               continue;
           }
           $value = $attrObj->getValue();
