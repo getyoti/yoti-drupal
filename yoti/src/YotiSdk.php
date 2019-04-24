cc6ddcf9eb6a9c6ff3c389b79a0fe75468af53b2
David Grayston <davidgrayston@googlemail.com>
2019-04-24 19:06:36 +0100

    SDK-924: Display user friendly error and log exception

---
+++ yoti/src/YotiSdk.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Drupal\yoti;
+
+use Yoti\YotiClient;
+
+require_once __DIR__ . '/../sdk/boot.php';
+
+/**
+ * Class YotiSdk.
+ *
+ * @package Drupal\yoti
+ */
+class YotiSdk implements YotiSdkInterface {
+
+  /**
+   * Yoti Drupal SDK identifier.
+   */
+  const SDK_IDENTIFIER = 'Drupal';
+
+  /**
+   * Yoti plugin config data.
+   *
+   * @var \Drupal\yoti\YotiConfigInterface
+   */
+  private $config;
+
+  /**
+   * YotiSDK constructor.
+   *
+   * @param \Drupal\yoti\YotiConfigInterface $config
+   *   Yoti plugin config data.
+   */
+  public function __construct(YotiConfigInterface $config) {
+    $this->config = $config;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getClient() {
+    return new YotiClient(
+      $this->config->getSdkId(),
+      $this->config->getPemContents(),
+      YotiClient::DEFAULT_CONNECT_API,
+      self::SDK_IDENTIFIER
+    );
+  }
+
+}
