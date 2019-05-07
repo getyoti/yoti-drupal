604c37ed4dc6c5f515914ce7caf876eb958ebb1d
David Grayston <davidgrayston@googlemail.com>
2019-05-07 17:30:22 +0100

    SDK-942: Check coding standards before running tests

--- docker/run-tests.sh
+++ docker/run-tests.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 TARGET="drupal-7-dev"
 
+# Coding Standards
+docker-compose exec $TARGET ./vendor/bin/phpcs --standard=Drupal --ignore=*yoti/sdk* ./sites/all/modules/yoti
+
 # Enable Yoti module
 docker-compose exec $TARGET drush en simpletest -y
 
