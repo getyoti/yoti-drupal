d145747fc581e8b701251c21982cb09e26d3e860
David Grayston <davidgrayston@googlemail.com>
2019-05-07 16:46:00 +0100

    SDK-941: Move coding standards check to separate branch

--- docker/run-tests.sh
+++ docker/run-tests.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
-docker-compose up -d drupal-7-dev
+TARGET="drupal-7-dev"
 
-# Coding Standards
-docker-compose exec drupal-7-dev ./vendor/bin/phpcs --standard=Drupal --ignore=*yoti/sdk* ./sites/all/modules/yoti
+# Enable Yoti module
+docker-compose exec $TARGET drush en simpletest -y
+
+# Run Tests.
+docker-compose exec $TARGET sudo -u www-data -E php ./scripts/run-tests.sh --verbose --color --url https://localhost --php /usr/local/bin/php Yoti
