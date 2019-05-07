8c69b4202007b4c06263b178aeece5ff70a6c78b
David Grayston <davidgrayston@googlemail.com>
2019-05-07 12:18:48 +0100

    SDK-789: Update composer installation and remove unneeded Drupal installation

--- docker/run-tests.sh
+++ docker/run-tests.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 docker-compose up -d drupal-8-dev
+sleep 10
 
 # Coding Standards
 docker-compose exec drupal-8-dev ./vendor/bin/phpcs --standard=Drupal --ignore=*/var/www/html/modules/yoti/sdk* ./modules/yoti
