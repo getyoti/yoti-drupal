5cbfad9d034b130f1f9a6549a55ba51989ab5dee
David Grayston <davidgrayston@googlemail.com>
2019-05-08 16:05:34 +0100

    SDK-958: Create test pem file to prevent errors during test run

--- docker/install-drupal.sh
+++ docker/install-drupal.sh
@@ -13,5 +13,8 @@ sleep 10
 # Install Drupal
 docker-compose exec $TARGET drush site:install -y
 
+# Set private file path
+docker-compose exec $TARGET drush vset file_private_path /var/www/private
+
 # Enable Yoti module
 docker-compose exec $TARGET drush en yoti -y
