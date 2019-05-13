584f1006809960962749257080c27d2c0b285378
David Grayston <davidgrayston@googlemail.com>
2019-05-13 14:18:23 +0100

    SDK-960: Reorganise access callbacks

--- yoti/src/Form/YotiUnlinkForm.php
+++ yoti/src/Form/YotiUnlinkForm.php
@@ -5,6 +5,9 @@ namespace Drupal\yoti\Form;
 use Drupal\Core\Form\FormStateInterface;
 use Drupal\Core\Form\ConfirmFormBase;
 use Drupal\Core\Url;
+use Drupal\yoti\Models\YotiUserModel;
+use Drupal\Core\Access\AccessResult;
+use Drupal\Core\Session\AccountInterface;
 
 /**
  * Class YotiUnlinkForm.
@@ -12,6 +15,21 @@ use Drupal\Core\Url;
  * @package Drupal\yoti\Form
  */
 class YotiUnlinkForm extends ConfirmFormBase {
+
+  /**
+   * Check that the current account is linked.
+   *
+   * @param \Drupal\Core\Session\AccountInterface $account
+   *   Run access checks for this account.
+   *
+   * @return \Drupal\Core\Access\AccessResult
+   *   If account is linked isAllowed() will be TRUE, otherwise
+   *   isNeutral() will be TRUE.
+   */
+  public static function access(AccountInterface $account) {
+    $db_profile = YotiUserModel::getYotiUserById($account->id());
+    return AccessResult::allowedIf(!empty($db_profile));
+  }
 
   /**
    * {@inheritdoc}
