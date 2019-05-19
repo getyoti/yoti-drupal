4c8d72e68ea23c217814743e8dfb5e539116ab42
David Grayston <davidgrayston@googlemail.com>
2019-05-20 00:31:51 +0100

    SDK-967: Test coverage for linked and unlinked user profiles

--- yoti/tests/src/Functional/YotiProfileTest.php
+++ yoti/tests/src/Functional/YotiProfileTest.php
@@ -2,6 +2,9 @@
 
 namespace Drupal\Tests\yoti\Functional;
 
+use Drupal\yoti\YotiHelper;
+use Yoti\Entity\Profile;
+
 /**
  * Tests the user profile.
  *
@@ -10,23 +13,45 @@ namespace Drupal\Tests\yoti\Functional;
 class YotiProfileTest extends YotiBrowserTestBase {
 
   /**
-   * Test user profile.
+   * Test profile for linked users.
    */
-  public function testProfile() {
+  public function testProfileLinked() {
     $this->drupalLogin($this->linkedUser);
     $this->drupalGet('user');
 
     $assert = $this->assertSession();
 
-    foreach (yoti_map_params() as $field => $label) {
-      $assert->elementExists('xpath', "//*[@class='label'][contains(text(),'{$label}')]");
+    // Check profile data without selfie.
+    $profileData = yoti_map_params();
+    unset($profileData[YotiHelper::ATTR_SELFIE_FILE_NAME]);
+    unset($profileData[Profile::ATTR_SELFIE]);
+    foreach ($profileData as $label) {
+      $assert->responseContains($label . ' value');
     }
 
+    // Check unlink button is present.
     $assert->elementTextContains(
       'css',
       "#yoti-unlink-button[href='/yoti/unlink']",
       'Unlink Yoti account'
     );
+
+    // Check selfie image is present.
+    $assert->elementExists('css', "img[src*='/yoti/bin-file/selfie'][width='100']");
+    $this->drupalGet('yoti/bin-file/selfie');
+    $assert->responseContains('test_selfie_contents');
+  }
+
+  /**
+   * Test profile for unlinked users.
+   */
+  public function testProfileUnlinked() {
+    $this->drupalLogin($this->unlinkedUser);
+    $this->drupalGet('user');
+
+    foreach (yoti_map_params() as $label) {
+      $this->assertSession()->responseNotContains($label);
+    }
   }
 
 }
