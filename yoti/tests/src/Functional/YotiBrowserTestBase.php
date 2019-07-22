b4fddc0346d4f4de987182f38ee702d410cae885
David Grayston <davidgrayston@googlemail.com>
2019-07-22 11:15:57 +0100

    SDK-1066: Display corresponding user data on profiles

--- yoti/tests/src/Functional/YotiBrowserTestBase.php
+++ yoti/tests/src/Functional/YotiBrowserTestBase.php
@@ -27,6 +27,13 @@ class YotiBrowserTestBase extends BrowserTestBase {
   protected $unlinkedUser;
 
   /**
+   * Selfie file path.
+   *
+   * @var string
+   */
+  protected $selfieFilePath;
+
+  /**
    * Modules to enable.
    *
    * @var array
@@ -39,18 +46,56 @@ class YotiBrowserTestBase extends BrowserTestBase {
   public function setup() {
     parent::setup();
 
-    $this->linkedUser = $this->drupalCreateUser([
-      'access content',
-    ]);
-    \Drupal::database()->insert(YotiHelper::YOTI_USER_TABLE_NAME)->fields([
-      'uid' => $this->linkedUser->id(),
-      'identifier' => 'some-remember-me-id',
-      'data' => serialize([]),
-    ])->execute();
+    // Create linked user.
+    $this->createLinkedUser();
 
+    // Create unlinked user.
     $this->unlinkedUser = $this->drupalCreateUser([
       'access content',
     ]);
+  }
+
+  /**
+   * Create a linked Drupal User.
+   */
+  private function createLinkedUser() {
+    // Create linked user.
+    $this->linkedUser = $this->drupalCreateUser([
+      'access content',
+    ]);
+
+    // Generate test user data from known attributes.
+    foreach (yoti_map_params() as $field => $label) {
+      $user_data[$field] = $label . ' value';
+    }
+
+    // Create test selfie file.
+    mkdir(YotiHelper::uploadDir(), 0777, TRUE);
+    $this->selfieFilePath = YotiHelper::uploadDir() . DIRECTORY_SEPARATOR . 'test_selfie.jpg';
+    file_put_contents($this->selfieFilePath, 'test_selfie_contents');
+    $user_data[YotiHelper::ATTR_SELFIE_FILE_NAME] = basename($this->selfieFilePath);
+
+    \Drupal::database()->insert(YotiHelper::YOTI_USER_TABLE_NAME)->fields([
+      'uid' => $this->linkedUser->id(),
+      'identifier' => 'some-remember-me-id',
+      'data' => serialize($user_data),
+    ])->execute();
+
+  }
+
+  /**
+   * Teardown Tests.
+   */
+  public function teardown() {
+    // Cleanup selfie.
+    if (is_file($this->selfieFilePath)) {
+      unlink($this->selfieFilePath);
+    }
+
+    // Cleanup private directory.
+    rmdir(YotiHelper::uploadDir());
+
+    parent::teardown();
   }
 
 }
