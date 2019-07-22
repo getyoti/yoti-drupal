483169c0311a34aced758184fe72953a13deaad6
David Grayston <davidgrayston@googlemail.com>
2019-07-22 11:15:57 +0100

    SDK-1066: Display corresponding user data on profiles

---
+++ yoti/tests/src/Functional/YotiProfileTest.php
@@ -0,0 +1,192 @@
+<?php
+
+namespace Drupal\Tests\yoti\Functional;
+
+use Drupal\yoti\YotiHelper;
+use Yoti\ActivityDetails;
+use Symfony\Component\Yaml\Yaml;
+
+/**
+ * Tests the user profile.
+ *
+ * @group yoti
+ */
+class YotiProfileTest extends YotiBrowserTestBase {
+
+  /**
+   * Test profile for linked users.
+   */
+  public function testProfileLinked() {
+    $this->drupalLogin($this->linkedUser);
+    $this->drupalGet('user');
+    $this->assertProfileFields(yoti_map_params());
+
+    // Check unlink button is present.
+    $this->assertSession()->elementTextContains(
+      'css',
+      "#yoti-unlink-button[href='/yoti/unlink']",
+      'Unlink Yoti account'
+    );
+  }
+
+  /**
+   * Test viewing profile as user with permission.
+   */
+  public function testProfileLinkedAsUserWithPermission() {
+    $userWithUserProfilePermission = $this->drupalCreateUser([
+      'access user profiles',
+    ]);
+    $this->drupalLogin($userWithUserProfilePermission);
+    $this->drupalGet('user/' . $this->linkedUser->id());
+    $this->assertProfileFields(yoti_map_params());
+
+    // Check unlink button is not present.
+    $this->assertSession()->responseNotContains('/yoti/unlink');
+  }
+
+  /**
+   * Test viewing profile as user without permission.
+   */
+  public function testProfileLinkedAsUserWithoutPermission() {
+    $userWithoutUserProfilePermission = $this->drupalCreateUser();
+    $this->drupalLogin($userWithoutUserProfilePermission);
+    $this->drupalGet('user/' . $this->linkedUser->id());
+    $this->assertSession()->statusCodeEquals(403);
+  }
+
+  /**
+   * Test profile with customised display.
+   */
+  public function testProfileCustomiseDisplay() {
+    $disable_fields = [
+      ActivityDetails::ATTR_FULL_NAME,
+      ActivityDetails::ATTR_GIVEN_NAMES,
+    ];
+
+    // Import config and hide user fields.
+    $this->importUserDisplayConfig();
+    $this->hideUserDisplayFields($disable_fields);
+
+    $this->drupalLogin($this->linkedUser);
+    $this->drupalGet('user');
+
+    // Create array of display and disabled fields.
+    $profile_data = yoti_map_params();
+    foreach ($disable_fields as $disable_field) {
+      unset($profile_data[$disable_field]);
+      $disabled_data[$disable_field] = yoti_map_params()[$disable_field];
+    }
+
+    $this->assertProfileFields($profile_data);
+    $this->assertNotProfileFields($disabled_data);
+  }
+
+  /**
+   * Test profile for unlinked users.
+   */
+  public function testProfileUnlinked() {
+    $this->drupalLogin($this->unlinkedUser);
+    $this->drupalGet('user');
+    $this->assertNotProfileFields(yoti_map_params());
+  }
+
+  /**
+   * Assert that the provided profile fields are present on profile.
+   *
+   * @param array $profile_data
+   *   Array of profile data to check.
+   */
+  private function assertProfileFields(array $profile_data) {
+    $assert = $this->assertSession();
+
+    // Check profile data without selfie.
+    unset($profile_data[YotiHelper::ATTR_SELFIE_FILE_NAME]);
+    unset($profile_data[ActivityDetails::ATTR_SELFIE]);
+
+    foreach ($profile_data as $key => $label) {
+      $assert->elementExists('css', '#yoti-profile-' . $key);
+      $assert->responseContains($label . ' value');
+    }
+
+    // Check selfie image is present.
+    $selfie_selector = "img[src*='/yoti/bin-file/selfie'][width='100']";
+    $assert->elementExists('css', $selfie_selector);
+
+    // Visit selfie using img src attribute.
+    $selfie_src_attr = $this
+      ->getSession()
+      ->getPage()
+      ->find('css', $selfie_selector)
+      ->getAttribute('src');
+    $selfie_url = htmlspecialchars_decode($selfie_src_attr);
+
+    $path = parse_url($selfie_url, PHP_URL_PATH);
+    parse_str(parse_url($selfie_url, PHP_URL_QUERY), $query_params);
+
+    $this->drupalGet(trim($path, '/'), ['query' => $query_params]);
+    $assert->responseContains('test_selfie_contents');
+
+    // Go back to previous page.
+    $this->getSession()->back();
+  }
+
+  /**
+   * Assert that the provided profile fields are not present on profile.
+   *
+   * @param array $profile_data
+   *   Array of profile data to check.
+   */
+  private function assertNotProfileFields(array $profile_data) {
+    foreach ($profile_data as $label) {
+      $this->assertSession()->responseNotContains($label);
+    }
+  }
+
+  /**
+   * Get the user display configuration.
+   *
+   * @return \Drupal\Core\Config\Config
+   *   Editable configuration object.
+   */
+  private function getUserDisplayConfig() {
+    return \Drupal::service('config.factory')
+      ->getEditable('core.entity_view_display.user.user.default');
+  }
+
+  /**
+   * Import the default user display configuration.
+   */
+  private function importUserDisplayConfig() {
+    $config = $this->getUserDisplayConfig();
+
+    $yaml = file_get_contents(__DIR__ . '/../../fixtures/config/core.entity_view_display.user.user.default.yml');
+
+    $config
+      ->setData(Yaml::parse($yaml))
+      ->save();
+  }
+
+  /**
+   * Hide the specified field from user display.
+   *
+   * @param array $field_names
+   *   Fields to hide from user display.
+   */
+  private function hideUserDisplayFields(array $field_names) {
+    $config = $this->getUserDisplayConfig();
+
+    foreach ($field_names as $field_name) {
+      $config
+        ->set('hidden.' . $field_name, TRUE)
+        ->save();
+
+      $content = $config->get('content');
+      unset($content[$field_name]);
+
+      $config
+        ->set('content', $content)
+        ->save();
+    }
+  }
+
+}
