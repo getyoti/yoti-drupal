f9f17246ed618d4e99223e212bc7d5ec96e5ced4
David Grayston <davidgrayston@googlemail.com>
2019-08-07 23:13:22 +0100

    SDK-887: Run code sniffer in module directory

--- docker/run-tests.sh
+++ docker/run-tests.sh
@@ -3,7 +3,7 @@ docker-compose up -d drupal-8-dev
 sleep 10
 
 # Coding Standards
-docker-compose exec drupal-8-dev ./vendor/bin/phpcs ./modules/yoti
+docker-compose exec drupal-8-dev sh -c "cd modules/yoti && ../../vendor/bin/phpcs"
 
 # Run Unit Tests
 docker-compose exec drupal-8-dev sudo -u www-data ./vendor/bin/phpunit
