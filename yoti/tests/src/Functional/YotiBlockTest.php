847f2f8c155c731155f6a84aeddf23032e38968e
David Grayston <davidgrayston@googlemail.com>
2020-04-24 18:08:30 +0100

    SDK-1535: Add schema for block settings

--- yoti/tests/src/Functional/YotiBlockTest.php
+++ yoti/tests/src/Functional/YotiBlockTest.php
@@ -52,4 +52,31 @@ class YotiBlockTest extends YotiBrowserTestBase {
     $assert->responseMatches('~"label":.*?"Link to Yoti"~');
   }
 
+  /**
+   * Test Yoti block with custom settings.
+   */
+  public function testYotiBlockWithCustomSettings() {
+    // Place the block with custom settings.
+    $this->drupalPlaceBlock('yoti_block', [
+      'button_text' => 'Some Button Text',
+      'scenario_id' => 'some_custom_scenario_id',
+    ]);
+
+    // Load the homepage.
+    $this->drupalGet('<front>');
+
+    // Check the block has been placed.
+    $assert = $this->assertSession();
+    $assert->elementExists('css', "div[id='yoti-button-yoti-block']");
+    $assert->elementExists('css', 'script[src*=\'js/browser-loader.js\']');
+    $assert->responseMatches('~"domId":.*?"yoti-button-yoti-block"~');
+    $assert->responseMatches('~"scenarioId":.*?"some_custom_scenario_id"~');
+    $assert->responseMatches('~"clientSdkId":.*?"test_sdk_id"~');
+    $assert->responseMatches('~"label":.*?"Some Button Text"~');
+
+    $this->drupalLogin($this->unlinkedUser);
+    $this->drupalGet('<front>');
+    $assert->responseMatches('~"label":.*?"Some Button Text"~');
+  }
+
 }
