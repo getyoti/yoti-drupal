923c655b795b2576b651ae763f522e2f4822d38f
David Grayston <davidgrayston@googlemail.com>
2020-04-24 19:40:33 +0100

    SDK-1535: Ensure button IDs are unique by using block ID

--- yoti/tests/src/Functional/YotiBlockTest.php
+++ yoti/tests/src/Functional/YotiBlockTest.php
@@ -33,16 +33,17 @@ class YotiBlockTest extends YotiBrowserTestBase {
    */
   public function testYotiBlock() {
     // Place the block.
-    $this->drupalPlaceBlock('yoti_block');
+    $block = $this->drupalPlaceBlock('yoti_block');
+    $block_id = $block->id();
 
     // Load the homepage.
     $this->drupalGet('<front>');
 
     // Check the block has been placed.
     $assert = $this->assertSession();
-    $assert->elementExists('css', "div[id='yoti-button-yoti-block']");
+    $assert->elementExists('css', "div[id='yoti-button-" . $block_id . "']");
     $assert->elementExists('css', 'script[src*=\'js/browser-loader.js\']');
-    $assert->responseMatches('~"domId":.*?"yoti-button-yoti-block"~');
+    $assert->responseMatches('~"domId":.*?"yoti-button-' . $block_id . '"~');
     $assert->responseMatches('~"scenarioId":.*?"test_scenario_id"~');
     $assert->responseMatches('~"clientSdkId":.*?"test_sdk_id"~');
     $assert->responseMatches('~"label":.*?"Use Yoti"~');
@@ -57,19 +58,20 @@ class YotiBlockTest extends YotiBrowserTestBase {
    */
   public function testYotiBlockWithCustomSettings() {
     // Place the block with custom settings.
-    $this->drupalPlaceBlock('yoti_block', [
+    $block = $this->drupalPlaceBlock('yoti_block', [
       'button_text' => 'Some Button Text',
       'scenario_id' => 'some_custom_scenario_id',
     ]);
+    $block_id = $block->id();
 
     // Load the homepage.
     $this->drupalGet('<front>');
 
     // Check the block has been placed.
     $assert = $this->assertSession();
-    $assert->elementExists('css', "div[id='yoti-button-yoti-block']");
+    $assert->elementExists('css', "div[id='yoti-button-" . $block_id . "']");
     $assert->elementExists('css', 'script[src*=\'js/browser-loader.js\']');
-    $assert->responseMatches('~"domId":.*?"yoti-button-yoti-block"~');
+    $assert->responseMatches('~"domId":.*?"yoti-button-' . $block_id . '"~');
     $assert->responseMatches('~"scenarioId":.*?"some_custom_scenario_id"~');
     $assert->responseMatches('~"clientSdkId":.*?"test_sdk_id"~');
     $assert->responseMatches('~"label":.*?"Some Button Text"~');
@@ -77,6 +79,38 @@ class YotiBlockTest extends YotiBrowserTestBase {
     $this->drupalLogin($this->unlinkedUser);
     $this->drupalGet('<front>');
     $assert->responseMatches('~"label":.*?"Some Button Text"~');
+  }
+
+  /**
+   * Test Yoti block placed twice.
+   */
+  public function testYotiBlockPlacedTwice() {
+    // Place the block.
+    $block1 = $this->drupalPlaceBlock('yoti_block');
+    $block1_id = $block1->id();
+
+    // Place the second block with custom settings.
+    $block2 = $this->drupalPlaceBlock('yoti_block', [
+      'button_text' => 'Some Other Button Text',
+      'scenario_id' => 'some_other_custom_scenario_id',
+    ]);
+    $block2_id = $block2->id();
+
+    // Load the homepage.
+    $this->drupalGet('<front>');
+
+    // Check the blocks have been placed.
+    $assert = $this->assertSession();
+
+    $assert->elementExists('css', "div[id='yoti-button-" . $block1_id . "']");
+    $assert->responseMatches('~"domId":.*?"yoti-button-' . $block1_id . '"~');
+    $assert->responseMatches('~"scenarioId":.*?"test_scenario_id"~');
+    $assert->responseMatches('~"label":.*?"Use Yoti"~');
+
+    $assert->elementExists('css', "div[id='yoti-button-" . $block2_id . "']");
+    $assert->responseMatches('~"domId":.*?"yoti-button-' . $block2_id . '"~');
+    $assert->responseMatches('~"scenarioId":.*?"some_other_custom_scenario_id"~');
+    $assert->responseMatches('~"label":.*?"Some Other Button Text"~');
   }
 
 }
