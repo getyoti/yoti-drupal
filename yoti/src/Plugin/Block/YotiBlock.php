2cb7ebf7303451ce8f050993d973d7430e1a438a
David Grayston <davidgrayston@googlemail.com>
2020-04-24 17:24:04 +0100

    SDK-1535: Allow scenario ID and button text to be set per block

--- yoti/src/Plugin/Block/YotiBlock.php
+++ yoti/src/Plugin/Block/YotiBlock.php
@@ -5,6 +5,7 @@ namespace Drupal\yoti\Plugin\Block;
 use Drupal\Component\Utility\Html;
 use Drupal\Core\Block\BlockBase;
 use Drupal\Core\Cache\Cache;
+use Drupal\Core\Form\FormStateInterface;
 use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
 use Drupal\Core\Session\AccountInterface;
 use Drupal\yoti\Models\YotiUserModel;
@@ -21,6 +22,26 @@ use Symfony\Component\DependencyInjection\ContainerInterface;
  * )
  */
 class YotiBlock extends BlockBase implements ContainerFactoryPluginInterface {
+  /**
+   * Config key for Scenario ID.
+   */
+  private const CONFIG_SCENARIO_ID = 'scenario_id';
+
+  /**
+   * Config key for Button Text.
+   */
+  private const CONFIG_BUTTON_TEXT = 'button_text';
+
+  /**
+   * Default button text for new users.
+   */
+  private const NEW_USER_BUTTON_TEXT = YotiHelper::YOTI_LINK_BUTTON_DEFAULT_TEXT;
+
+  /**
+   * Default button text for linked users.
+   */
+  private const EXISTING_USER_BUTTON_TEXT = 'Link to Yoti';
+
   /**
    * Yoti configuration.
    *
@@ -80,6 +101,93 @@ class YotiBlock extends BlockBase implements ContainerFactoryPluginInterface {
   }
 
   /**
+   * {@inheritdoc}
+   */
+  public function blockForm($form, FormStateInterface $form_state) {
+    $form = parent::blockForm($form, $form_state);
+    $configuration = $this->getConfiguration();
+
+    $form[self::CONFIG_BUTTON_TEXT] = [
+      '#type' => 'textfield',
+      '#title' => t('Button Text'),
+      '#description' => t('Leave empty to use the default button text'),
+      '#required' => FALSE,
+      '#default_value' => !empty($configuration[self::CONFIG_BUTTON_TEXT]) ? $configuration[self::CONFIG_BUTTON_TEXT] : '',
+    ];
+
+    $form[self::CONFIG_SCENARIO_ID] = [
+      '#type' => 'textfield',
+      '#title' => t('Scenario ID'),
+      '#description' => t('Leave empty to use the default Scenario ID'),
+      '#required' => FALSE,
+      '#default_value' => !empty($configuration[self::CONFIG_SCENARIO_ID]) ? $configuration[self::CONFIG_SCENARIO_ID] : '',
+    ];
+
+    return $form;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function blockSubmit($form, FormStateInterface $form_state) {
+    $this->setConfigurationValue(
+      self::CONFIG_BUTTON_TEXT,
+      trim($form_state->getValue(self::CONFIG_BUTTON_TEXT))
+    );
+
+    $this->setConfigurationValue(
+      self::CONFIG_SCENARIO_ID,
+      trim($form_state->getValue(self::CONFIG_SCENARIO_ID))
+    );
+  }
+
+  /**
+   * Gets the Scenario ID for this block.
+   *
+   * @return string
+   *   The Scenario ID for this block.
+   */
+  private function getScenarioId(): string {
+    $config = $this->getConfiguration();
+    if (!empty($config[self::CONFIG_SCENARIO_ID])) {
+      return $config[self::CONFIG_SCENARIO_ID];
+    }
+
+    return $this->yotiConfig->getScenarioId();
+  }
+
+  /**
+   * Gets the button text for this block.
+   *
+   * @return string
+   *   The button text for this block.
+   */
+  private function getButtonText(): string {
+    $config = $this->getConfiguration();
+    if (!empty($config[self::CONFIG_BUTTON_TEXT])) {
+      return $config[self::CONFIG_BUTTON_TEXT];
+    }
+
+    return empty($this->currentUser->id()) ? self::NEW_USER_BUTTON_TEXT : self::EXISTING_USER_BUTTON_TEXT;
+  }
+
+  /**
+   * Determines that the user is linked to Yoti.
+   *
+   * @return bool
+   *   The user is linked to Yoti.
+   */
+  private function userIsLinked(): bool {
+    $userId = $this->currentUser->id();
+
+    if (!$userId) {
+      return FALSE;
+    }
+
+    return YotiUserModel::getYotiUserById($userId) ? TRUE : FALSE;
+  }
+
+  /**
    * Builds and returns the renderable array for this block plugin.
    *
    * If a block should not be rendered because it has no content, then this
@@ -98,24 +206,13 @@ class YotiBlock extends BlockBase implements ContainerFactoryPluginInterface {
       return [];
     }
 
-    // Set button text based on current user.
-    $userId = $this->currentUser->id();
-    if (!$userId) {
-      $button_text = YotiHelper::YOTI_LINK_BUTTON_DEFAULT_TEXT;
-      $is_linked = FALSE;
-    }
-    else {
-      $button_text = 'Link to Yoti';
-      $is_linked = YotiUserModel::getYotiUserById($userId) ? TRUE : FALSE;
-    }
-
     return [
       '#theme' => 'yoti_button',
       '#button_id' => Html::getUniqueId('yoti-button-' . $this->getPluginId()),
       '#client_sdk_id' => $this->yotiConfig->getClientSdkId(),
-      '#scenario_id' => $this->yotiConfig->getScenarioId(),
-      '#button_text' => $button_text,
-      '#is_linked' => $is_linked,
+      '#scenario_id' => $this->getScenarioId(),
+      '#button_text' => $this->getButtonText(),
+      '#is_linked' => $this->userIsLinked(),
       '#attached' => [
         'library' => [
           'yoti/yoti',
